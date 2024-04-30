15f80f0f1c07863cc84a3cf1abaafb47ec7ef3ba
smyleface18 <ca30850@gmail.com>
2024-04-29 21:30:57 -0500

    add funcionalidad de alert_price,  funciona correctamente

--- js/aplication.js
+++ js/aplication.js
@@ -1,4 +1,6 @@
 
+    //variables de fetch
+
 const url = "http://127.0.0.1:8000/price";
 const options = {
     method: "GET",
@@ -9,27 +11,76 @@ const options = {
     }
 }
 
-var cebolla
 
-
-    await fetch(url,options)
-        .then(res => res.json())
-        .then(data =>{
-            cebolla = data[0];})
-        .catch(error => console.error("Error al obtener los datos:", error));
-        
-     console.log(cebolla.price)  
     const value_desired = document.querySelector("#value_desired");
     const button = document.querySelector("#button");
-    console.log(value_desired)
-    var value 
-    button.addEventListener("click",() =>{
-        value = value_desired.value;
-        console.log(value)
-        if(value >= cebolla.price){
-            alert("PRECIO SATISFACTORIO")
-        }
+    const PRODUCT = document.querySelector("#producto"); 
+
+
+    localStorage.setItem("alert_reload", false)
+
+    if(localStorage.getItem("alert_reload")){
+        window.addEventListener("load", () =>{
+            let index_product = localStorage.getItem("index_product");
+            let value = localStorage.getItem("value_desired");
+            let myJson = localStorage.getItem("myJson");
+            console.log("funcionando reload alter")
+            if(index_product != undefined){
+                 if(myJson[index_product].price >= value ){
+                     console.log("entro")
+                     alert("price")
+                     value_desired.value = "";
+                 }
+                 else {
+                     value_desired.value = value;
+                 }
+            }
+         });
+
+    }
+
+
+    // llamada a la api
+
+    var myJson
+     await fetch(url,options)
+        .then(res => res.json())
+        .then(data =>{
+             myJson = data;})
+        .catch(error => console.error("Error al obtener los datos:", error));
         
+     localStorage.setItem("myJson", JSON.stringify(myJson));        
+
+
+    //funcionalidad de alert
+
+
+
+
+    PRODUCT.addEventListener("click",function(e)  {
+        localStorage.setItem("index_product", e.target.selectedIndex)
+        console.log(localStorage.getItem("index_product"))
     });
+
+
+
+        button.addEventListener("click",()=>{
+            let index_product = localStorage.getItem("index_product");
+            localStorage.setItem("value_desired", value_desired.value);
+            console.log(value_desired.value)
+            console.log(myJson[index_product].price)
+            if(myJson[index_product].price >= value_desired.value ){
+               
+                alert("price")
+                value_desired.value = "";
+            }
+            else{
+                localStorage.setItem("alert_reload",true);
+                console.log("entro")
+            }
+        });
+
+
+        
 
  
